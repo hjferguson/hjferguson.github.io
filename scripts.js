db2fd8eee55381e822c8a6f378d7e55a3b9ac05c
Harlan Ferguson <harlanjazz@gmail.com>
2023-04-10 22:49:25 -0400

    adjusting icon sizes, more adjustments to javascript animations

--- scripts.js
+++ scripts.js
@@ -1,52 +1,29 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-//animations
 document.addEventListener("DOMContentLoaded", function () {
     var projects = document.getElementsByClassName("project-box");
   
-    for (var i = 0; i < projects.length; i++) {
-      projects[i].classList.add("hidden");
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
     }
   
-    window.addEventListener("scroll", function () {
+    function toggleAnimation() {
       for (var i = 0; i < projects.length; i++) {
         if (isElementInViewport(projects[i])) {
           projects[i].classList.add("slide-in");
           projects[i].classList.remove("hidden");
+        } else {
+          projects[i].classList.remove("slide-in");
+          projects[i].classList.add("hidden");
         }
       }
-    });
-  });
-  
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-  
-    return (
-      rect.top < (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.bottom > 0 &&
-      rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
-      rect.right > 0
-    );
-  }
-  
-  window.addEventListener("scroll", function () {
-    for (var i = 0; i < projects.length; i++) {
-        if (isElementInViewport(projects[i])) {
-            projects[i].classList.add("slide-in");
-            projects[i].classList.remove("hidden");
-        } else {
-            projects[i].classList.remove("slide-in");
-            projects[i].classList.add("hidden");
-        }
     }
-});
   
+    window.addEventListener("scroll", toggleAnimation);
+    toggleAnimation();
+  });
   
