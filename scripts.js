7c9d52956fd0972ecf444e74153967d125342300
hjferguson <harlanjazz@gmail.com>
2023-04-11 08:19:50 -0400

    added if statement for screen size

--- scripts.js
+++ scripts.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var projects = document.getElementsByClassName("project-box");
-  
-    function isElementInViewport(el) {
-      var rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-    }
-  
-    function toggleAnimation() {
+  var projects = document.getElementsByClassName("project-box");
+
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function toggleAnimation() {
+    // Check if the screen width is greater than 767px
+    if (window.innerWidth > 767) {
       for (var i = 0; i < projects.length; i++) {
         if (isElementInViewport(projects[i])) {
           projects[i].classList.add("slide-in");
@@ -21,9 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
           projects[i].classList.add("hidden");
         }
       }
+    } else {
+      // If screen width is 767px or smaller, disable the animation and show all the elements
+      for (var i = 0; i < projects.length; i++) {
+        projects[i].classList.remove("hidden");
+      }
     }
-  
-    window.addEventListener("scroll", toggleAnimation);
-    toggleAnimation();
-  });
-  
+  }
+
+  window.addEventListener("scroll", toggleAnimation);
+  toggleAnimation();
+});
