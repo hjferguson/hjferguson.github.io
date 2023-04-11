b6f3e95131ad810f917cb744a1ca9a818fceab3d
Harlan Ferguson <harlanjazz@gmail.com>
2023-04-10 21:05:28 -0400

    adjusted formatting. forgot to link js... 🙃

--- scripts.js
+++ scripts.js
@@ -7,3 +7,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+//animations
+document.addEventListener("DOMContentLoaded", function () {
+    var projects = document.getElementsByClassName("project-box");
+  
+    for (var i = 0; i < projects.length; i++) {
+      projects[i].classList.add("hidden");
+    }
+  
+    window.addEventListener("scroll", function () {
+      for (var i = 0; i < projects.length; i++) {
+        if (isElementInViewport(projects[i])) {
+          projects[i].classList.add("slide-in");
+          projects[i].classList.remove("hidden");
+        }
+      }
+    });
+  });
+  
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+  
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
