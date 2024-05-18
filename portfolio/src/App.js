7b644d4bb2ec87e3a37a23fccdc48237e71ee32e
hjferguson <harlanjazz@gmail.com>
2024-05-17 22:50:04 -0400

    removes node_modules from tracking

---
+++ portfolio/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+// import About from './components/About';
+// import Projects from './components/Projects';
+// import ProjectCategory from './components/ProjectCategory';
+// import Contact from './components/Contact';
+// import Resume from './components/Resume';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          {/* <Route path="/" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:category" element={<ProjectCategory />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => (
+  <header>
+    <nav className="navbar">
+      <div className="nav-logo">
+        <Link to="/">Harlan Jazz Ferguson</Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/">About</Link>
+        <Link to="/projects">Projects</Link>
+        <a href="/docs/assets/Harlan_Ferguson_Resume.pdf" target="_blank" rel="noreferrer">Resume</a>
+        <Link to="/contact">Contact</Link>
+      </div>
+      <div className="nav-social">
+        <a href="https://www.linkedin.com/in/harlan-ferguson-09b55010b/" target="_blank" rel="noreferrer">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="https://github.com/hjferguson" target="_blank" rel="noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://www.instagram.com/harlerino/" target="_blank" rel="noreferrer">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
+    </nav>
+  </header>
+);
+
+export default Header;
